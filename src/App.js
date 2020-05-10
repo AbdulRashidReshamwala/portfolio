7de2f00755692807a8b3397bfd2e888dbee1920f
Abdul Rashid <ar1242112@gmail.com>
2020-05-10 21:39:52 +0530

    added link to resume

--- src/App.js
+++ src/App.js
@@ -1,64 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Navbar, Image, Row, Nav } from "react-bootstrap";
-import "./navbar.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <Router>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="dark"
-        variant="dark"
-        className="nav-main"
-      >
-        <Navbar.Brand>
-          <Link className="nav-button" to="/">
-            <Row>
-              <Image
-                fluid
-                className="nav-img"
-                src={`${window.location.origin}/home.png`}
-              ></Image>
-              <h3>Home</h3>
-            </Row>
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse
-          id="responsive-navbar-nav"
-          className="justify-content-around"
-        >
-          <Nav.Link>
-            <Link className="nav-button" to="/projects">
-              <Row>
-                <Image
-                  fluid
-                  className="nav-img"
-                  src={`${window.location.origin}/bulb.png`}
-                ></Image>
-                <h3>Projects</h3>
-              </Row>
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link className="nav-button" to="/skills">
-              <Row>
-                <Image
-                  fluid
-                  className="nav-img"
-                  src={`${window.location.origin}/react.png`}
-                ></Image>
-                <h3>Skills</h3>
-              </Row>
-            </Link>
-          </Nav.Link>
-        </Navbar.Collapse>
-      </Navbar>
-
-      {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+      <NavBar />
       <Switch>
         <Route path="/projects">
           <h1>Project</h1>
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Navbar, Image, Row, Nav } from "react-bootstrap";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      className="nav-main"
+    >
+      <Navbar.Brand>
+        <Link className="nav-button" to="/">
+          <Row>
+            <Image
+              fluid
+              className="nav-img"
+              src={`${window.location.origin}/home.png`}
+            ></Image>
+            <h3>Home</h3>
+          </Row>
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        className="justify-content-around"
+      >
+        <Nav.Link>
+          <Link className="nav-button" to="/projects">
+            <Row>
+              <Image
+                fluid
+                className="nav-img"
+                src={`${window.location.origin}/bulb.png`}
+              ></Image>
+              <h3>Projects</h3>
+            </Row>
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link className="nav-button" to="/skills">
+            <Row>
+              <Image
+                fluid
+                className="nav-img"
+                src={`${window.location.origin}/react.png`}
+              ></Image>
+              <h3>Skills</h3>
+            </Row>
+          </Link>
+        </Nav.Link>
+        <Nav.Link
+          href="https://drive.google.com/file/d/1sBT1KCuKiJbOaYgko8EC3rvzp1E9ESKz/view"
+          target="_blank"
+        >
+          <a className="nav-button">
+            <Row>
+              <Image
+                fluid
+                className="nav-img"
+                src={`${window.location.origin}/resume.png`}
+              ></Image>
+              <h3>Resume</h3>
+            </Row>
+          </a>
+        </Nav.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
